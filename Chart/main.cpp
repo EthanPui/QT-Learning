429e99e4c4fc30da29b4631a29e8676ef0524de9
EthanPui <ethanpui@gmail.com>
2023-07-25 12:32:16 +1000

    Added Include

--- Chart/main.cpp
+++ Chart/main.cpp
@@ -2,7 +2,17 @@
 
 #include <QApplication>
 
-
+#include <QtWidgets/QMainWindow>
+#include <QtCharts/QChartView>
+#include <QtCharts/QBarSeries>
+#include <QtCharts/QBarSet>
+#include <QtCharts/QLegend>
+#include <QtCharts/QBarCategoryAxis>
+#include <QtCharts/QHorizontalStackedBarSeries>
+#include <QtCharts/QLineSeries>
+#include <QtCharts/QCategoryAxis>
+#include <QtCharts/QPieSeries>
+#include <QtCharts/QPieSlice>
 
 int main(int argc, char *argv[])
 {
